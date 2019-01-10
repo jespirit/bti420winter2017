e6b2c8c724dc3f6426c46df9ade28d83f81ec420
Jeffrey Espiritu <jespiritu@myseneca.ca>
2019-01-10 14:12:36 -0500

    Add assignment 3

---
+++ Assignments/Assignment3/Assignment3/Controllers/EmployeesController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment3.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        Manager m = new Manager();
+
+        // GET: Employees
+        public ActionResult Index()
+        {
+            return View(m.EmployeeGetAll());
+        }
+
+        // GET: Employees/Details/5
+        public ActionResult Details(int? id)
+        {
+            var e = m.EmployeeGetById(id.GetValueOrDefault());
+
+            if (e == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(e);
+            }
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Create
+        [HttpPost]
+        public ActionResult Create(EmployeeAdd newItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(newItem);
+            }
+
+            var addedItem = m.EmployeeAdd(newItem);
+
+            if (addedItem == null)
+            {
+                // unable to add, display user-entered fields
+                return View(newItem);
+            }
+            else
+            {
+                return RedirectToAction("Details", addedItem);
+            }
+        }
+
+        // GET: Employees/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            var e = m.EmployeeGetById(id.GetValueOrDefault());
+
+            if (e == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(m.mapper.Map<EmployeeEditProfileInfoForm>(e));
+            }
+        }
+
+        // POST: Employees/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int? id, EmployeeEditProfileInfo newItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("edit", new { id = newItem.EmployeeId });
+            }
+
+            if (newItem.EmployeeId != id)
+            {
+                // data tampering
+                return RedirectToAction("index");
+            }
+
+            var editItem = m.EmployeeEditProfileInfo(newItem);
+
+            if (editItem == null)
+            {
+                return RedirectToAction("edit", new { id = editItem.EmployeeId });
+            }
+            else
+            {
+                return RedirectToAction("details", new { id = editItem.EmployeeId });
+            }
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            var itemToDelete = m.EmployeeGetById(id.GetValueOrDefault());
+
+            if (itemToDelete == null)
+            {
+                return RedirectToAction("index");
+            }
+            else
+            {
+                return View(itemToDelete);
+            }
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int? id, FormCollection collection)
+        {
+            // Trust the POST :id
+            var result = m.EmployeeDelete(id.GetValueOrDefault());
+
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ Assignments/Assignment3/Assignment3/Controllers/Manager.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// new...
+using AutoMapper;
+using Assignment3.Models;
+
+namespace Assignment3.Controllers
+{
+    public class Manager
+    {
+        // Reference to the data context
+        private DataContext ds = new DataContext();
+
+        // AutoMapper components
+        MapperConfiguration config;
+        public IMapper mapper;
+
+        public Manager()
+        {
+            // If necessary, add more constructor code here...
+
+            // Configure the AutoMapper components
+            config = new MapperConfiguration(cfg =>
+            {
+                // Define the mappings below, for example...
+                // cfg.CreateMap<SourceType, DestinationType>();
+                // cfg.CreateMap<Employee, EmployeeBase>();
+
+                cfg.CreateMap<Employee, EmployeeBase>();
+
+                // Creating a new employee
+                cfg.CreateMap<EmployeeAdd, Employee>();
+
+                // Displaying 
+                //cfg.CreateMap<EmployeeAdd, EmployeeBase>();
+
+                // To browser form
+                cfg.CreateMap<EmployeeBase, EmployeeEditProfileInfoForm>();
+
+            });
+
+            mapper = config.CreateMapper();
+
+            // Turn off the Entity Framework (EF) proxy creation features
+            // We do NOT want the EF to track changes - we'll do that ourselves
+            ds.Configuration.ProxyCreationEnabled = false;
+
+            // Also, turn off lazy loading...
+            // We want to retain control over fetching related objects
+            ds.Configuration.LazyLoadingEnabled = false;
+        }
+
+        // Add methods below
+        // Controllers will call these methods
+        // Ensure that the methods accept and deliver ONLY view model objects and collections
+        // The collection return type is almost always IEnumerable<T>
+
+        // Suggested naming convention: Entity + task/action
+        // For example:
+        // ProductGetAll()
+        // ProductGetById()
+        // ProductAdd()
+        // ProductEdit()
+        // ProductDelete()
+
+        public IEnumerable<EmployeeBase> EmployeeGetAll()
+        {
+            return mapper.Map<IEnumerable<EmployeeBase>>(ds.Employees);
+        }
+
+        public EmployeeBase EmployeeGetById(int id)
+        {
+            var e = ds.Employees.Find(id);
+
+            if (e == null)
+            {
+                return null;
+            }
+            else
+            {
+                return mapper.Map<EmployeeBase>(e);
+            }
+        }
+
+        public EmployeeBase EmployeeAdd(EmployeeAdd newItem)
+        {
+            var addedItem = ds.Employees.Add(mapper.Map<Employee>(newItem));
+
+            if (addedItem == null)
+            {
+                return null;
+            }
+            else
+            {
+                // Display "details"
+                return mapper.Map<EmployeeBase>(addedItem);
+            }
+        }
+
+        public EmployeeBase EmployeeEditProfileInfo(EmployeeEditProfileInfo editItem)
+        {
+            var e = ds.Employees.Find(editItem.EmployeeId);
+
+            if (e == null)
+            {
+                return null;
+            }
+            else
+            {
+                ds.Entry(e).CurrentValues.SetValues(editItem);
+                ds.SaveChanges();
+
+                return mapper.Map<EmployeeBase>(e);
+            }
+        }
+
+        public bool EmployeeDelete(int id)
+        {
+            var e = ds.Employees.Find(id);
+
+            if (e == null)
+            {
+                return false;
+            }
+            else
+            {
+                ds.Employees.Remove(e);
+                ds.SaveChanges();
+
+                return true;
+            }
+        }
+    }
+}
