b6ee9c4797f7373a07390a91a788d22c31415ac9
Jeffrey Espiritu <jespiritu@myseneca.ca>
2019-01-11 15:22:33 -0500

    Copy files for assignment 4

---
+++ Assignments/Assignment4/Assignment4/Controllers/Employees_vm.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Assignment4.Controllers
+{
+    public class EmployeeAdd
+    {
+        [Required]
+        [StringLength(20)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string FirstName { get; set; }
+
+        [StringLength(30)]
+        public string Title { get; set; }
+
+        public int? ReportsTo { get; set; }
+
+        public DateTime? BirthDate { get; set; }
+
+        public DateTime? HireDate { get; set; }
+
+        [StringLength(70)]
+        public string Address { get; set; }
+
+        [StringLength(40)]
+        public string City { get; set; }
+
+        [StringLength(40)]
+        public string State { get; set; }
+
+        [StringLength(40)]
+        public string Country { get; set; }
+
+        [StringLength(10)]
+        public string PostalCode { get; set; }
+
+        [StringLength(24)]
+        public string Phone { get; set; }
+
+        [StringLength(24)]
+        public string Fax { get; set; }
+
+        [StringLength(60)]
+        public string Email { get; set; }
+    }
+
+    public class EmployeeBase : EmployeeAdd
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+    }
+
+    public class EmployeeEditProfileInfoForm
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string FirstName { get; set; }
+
+        [StringLength(30)]
+        public string Title { get; set; }
+
+        [StringLength(70)]
+        public string Address { get; set; }
+
+        [StringLength(40)]
+        public string City { get; set; }
+
+        [StringLength(40)]
+        public string State { get; set; }
+
+        [StringLength(40)]
+        public string Country { get; set; }
+
+        [StringLength(10)]
+        public string PostalCode { get; set; }
+
+        [StringLength(24)]
+        // Adds <input type="tel> attribute
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"\+?[ ]*[1-9]?[ ]*\-?[ ]*\(?[ ]*[1-9][ ]*(\d[ ]*){2}\)?[ ]*\-?[ ]*(\d[ ]*){3}-[ ]*(\d[ ]*){4}",
+            ErrorMessage = "Invalid Phone number")]
+        public string Phone { get; set; }
+
+        [StringLength(24)]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"\+?[ ]*[1-9]?[ ]*\-?[ ]*\(?[ ]*[1-9][ ]*(\d[ ]*){2}\)?[ ]*\-?[ ]*(\d[ ]*){3}-[ ]*(\d[ ]*){4}",
+            ErrorMessage = "Invalid Fax number")]
+        public string Fax { get; set; }
+
+        [StringLength(60)]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+
+    public class EmployeeEditProfileInfo
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        [StringLength(70)]
+        public string Address { get; set; }
+
+        [StringLength(40)]
+        public string City { get; set; }
+
+        [StringLength(40)]
+        public string State { get; set; }
+
+        [StringLength(40)]
+        public string Country { get; set; }
+
+        [StringLength(10)]
+        public string PostalCode { get; set; }
+
+        [StringLength(24)]
+        //[DataType(DataType.PhoneNumber, ErrorMessage = "Invalid Phone number")]
+        [RegularExpression(@"\+?[ ]*[1-9]?[ ]*\-?[ ]*\(?[ ]*[1-9][ ]*(\d[ ]*){2}\)?[ ]*\-?[ ]*(\d[ ]*){3}-[ ]*(\d[ ]*){4}",
+            ErrorMessage = "Invalid Phone number")]
+        public string Phone { get; set; }
+
+        [StringLength(24)]
+        // Specifies that a data field value is a well-formed phone number.
+        // Does NOT validate the phone number
+        //public class DataTypeAttribute : ValidationAttribute
+        //
+        // Summary:
+        //     Checks that the value of the data field is valid.
+        //
+        // Parameters:
+        //   value:
+        //     The data field value to validate.
+        //
+        // Returns:
+        //     true always.
+        //public override bool IsValid(object value);
+        //[DataType(DataType.PhoneNumber, ErrorMessage = "Invalid Fax number")]
+        [RegularExpression(@"\+?[ ]*[1-9]?[ ]*\-?[ ]*\(?[ ]*[1-9][ ]*(\d[ ]*){2}\)?[ ]*\-?[ ]*(\d[ ]*){3}-[ ]*(\d[ ]*){4}",
+            ErrorMessage = "Invalid Fax number")]
+        public string Fax { get; set; }
+
+        [StringLength(60)]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Invalid Email address")]
+        public string Email { get; set; }
+    }
+}
---
+++ Assignments/Assignment4/Assignment4/Controllers/Manager.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// new...
+using AutoMapper;
+using Assignment4.Models;
+
+namespace Assignment4.Controllers
+{
+    public class Manager
+    {
+        // Reference to the data context
+        private DataContext ds = new DataContext();
+
+        // AutoMapper components
+        MapperConfiguration config;
+        public IMapper mapper;
+
+        public Manager()
+        {
+            // If necessary, add more constructor code here...
+
+            // Configure the AutoMapper components
+            config = new MapperConfiguration(cfg =>
+            {
+                // Define the mappings below, for example...
+                // cfg.CreateMap<SourceType, DestinationType>();
+                // cfg.CreateMap<Employee, EmployeeBase>();
+
+                cfg.CreateMap<Employee, EmployeeBase>();
+
+                // Creating a new employee
+                cfg.CreateMap<EmployeeAdd, Employee>();
+
+                // Displaying 
+                //cfg.CreateMap<EmployeeAdd, EmployeeBase>();
+
+                // To browser form
+                cfg.CreateMap<EmployeeBase, EmployeeEditProfileInfoForm>();
+
+                cfg.CreateMap<Track, TrackBase>();
+            });
+
+            mapper = config.CreateMapper();
+
+            // Turn off the Entity Framework (EF) proxy creation features
+            // We do NOT want the EF to track changes - we'll do that ourselves
+            ds.Configuration.ProxyCreationEnabled = false;
+
+            // Also, turn off lazy loading...
+            // We want to retain control over fetching related objects
+            ds.Configuration.LazyLoadingEnabled = false;
+        }
+
+        // Add methods below
+        // Controllers will call these methods
+        // Ensure that the methods accept and deliver ONLY view model objects and collections
+        // The collection return type is almost always IEnumerable<T>
+
+        // Suggested naming convention: Entity + task/action
+        // For example:
+        // ProductGetAll()
+        // ProductGetById()
+        // ProductAdd()
+        // ProductEdit()
+        // ProductDelete()
+
+        public IEnumerable<EmployeeBase> EmployeeGetAll()
+        {
+            return mapper.Map<IEnumerable<EmployeeBase>>(ds.Employees);
+        }
+
+        public EmployeeBase EmployeeGetById(int id)
+        {
+            var e = ds.Employees.Find(id);
+
+            if (e == null)
+            {
+                return null;
+            }
+            else
+            {
+                return mapper.Map<EmployeeBase>(e);
+            }
+        }
+
+        public EmployeeBase EmployeeAdd(EmployeeAdd newItem)
+        {
+            var addedItem = ds.Employees.Add(mapper.Map<Employee>(newItem));
+
+            if (addedItem == null)
+            {
+                return null;
+            }
+            else
+            {
+                // Display "details"
+                return mapper.Map<EmployeeBase>(addedItem);
+            }
+        }
+
+        public EmployeeBase EmployeeEditProfileInfo(EmployeeEditProfileInfo editItem)
+        {
+            var e = ds.Employees.Find(editItem.EmployeeId);
+
+            if (e == null)
+            {
+                return null;
+            }
+            else
+            {
+                ds.Entry(e).CurrentValues.SetValues(editItem);
+                ds.SaveChanges();
+
+                return mapper.Map<EmployeeBase>(e);
+            }
+        }
+
+        public bool EmployeeDelete(int id)
+        {
+            var e = ds.Employees.Find(id);
+
+            if (e == null)
+            {
+                return false;
+            }
+            else
+            {
+                ds.Employees.Remove(e);
+                ds.SaveChanges();
+
+                return true;
+            }
+        }
+
+        public IEnumerable<TrackBase> TrackGetAll()
+        {
+            return mapper.Map<IEnumerable<TrackBase>>(ds.Tracks);
+        }
+
+        public IEnumerable<TrackBase> TrackGetAllSorted()
+        {
+            var tracks = ds.Tracks
+                .OrderBy(o => o.AlbumId).ThenBy(o => o.Name);
+
+            return mapper.Map<IEnumerable<TrackBase>>(tracks);
+        }
+
+        public IEnumerable<TrackBase> TrackGetAllPop()
+        {
+            // Pop, GenreId = 9
+            var tracks = ds.Tracks
+                .Join(ds.Genres, t => t.GenreId, g => g.GenreId,
+                (t, g) => new { Track = t, GenreName = g.Name })  // Composite object of Track + Genre
+                .Where(t_g => t_g.GenreName == "Pop")
+                .OrderBy(t_g => t_g.Track.Name)
+                .Select(t_g => t_g.Track);  // Get back 'Track'
+
+            return mapper.Map<IEnumerable<TrackBase>>(tracks);
+        }
+
+        public IEnumerable<TrackBase> TrackGetAllDeepPurple()
+        {
+            var tracks = ds.Tracks
+                // Same as t.Composer LIKE '%Jon Lord%'
+                .Where(t => t.Composer.Contains("Jon Lord"))
+                .OrderBy(t => t.TrackId);
+
+            return mapper.Map<IEnumerable<TrackBase>>(tracks);
+        }
+
+        public IEnumerable<TrackBase> TrackGetAllTop100Longest()
+        {
+            var tracks = ds.Tracks
+                .OrderByDescending(t => t.Milliseconds)
+                .Take(100);
+
+            return mapper.Map<IEnumerable<TrackBase>>(tracks);
+        }
+    }
+}
