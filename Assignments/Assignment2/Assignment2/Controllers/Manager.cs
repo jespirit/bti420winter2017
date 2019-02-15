25d1b693ee7a0329816038e1144bf1d81d7676ce
Jeffrey Espiritu <jespiritu@myseneca.ca>
2019-02-15 01:05:49 -0500

    Add assignment 2

---
+++ Assignments/Assignment2/Assignment2/Controllers/Manager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// new...
+using AutoMapper;
+using Assignment2.Models;
+
+namespace Assignment2.Controllers
+{
+    public class Manager
+    {
+        // Reference to the data context
+        private DataContext ds = new DataContext();
+
+        // AutoMapper components
+        MapperConfiguration config;
+        public IMapper mapper;
+
+        public Manager()
+        {
+            // If necessary, add more constructor code here...
+
+            // Configure the AutoMapper components
+            config = new MapperConfiguration(cfg =>
+            {
+                // Define the mappings below, for example...
+                // cfg.CreateMap<SourceType, DestinationType>();
+                // cfg.CreateMap<Employee, EmployeeBase>();
+
+                cfg.CreateMap<Employee, EmployeeAdd>();
+
+                cfg.CreateMap<Employee, EmployeeBase>();
+            });
+
+            mapper = config.CreateMapper();
+
+            // Turn off the Entity Framework (EF) proxy creation features
+            // We do NOT want the EF to track changes - we'll do that ourselves
+            ds.Configuration.ProxyCreationEnabled = false;
+
+            // Also, turn off lazy loading...
+            // We want to retain control over fetching related objects
+            ds.Configuration.LazyLoadingEnabled = false;
+        }
+
+        // Add methods below
+        // Controllers will call these methods
+        // Ensure that the methods accept and deliver ONLY view model objects and collections
+        // The collection return type is almost always IEnumerable<T>
+
+        // Suggested naming convention: Entity + task/action
+        // For example:
+        // ProductGetAll()
+        // ProductGetById()
+        // ProductAdd()
+        // ProductEdit()
+        // ProductDelete()
+
+        public IEnumerable<EmployeeBase> EmployeeGetAll()
+        {
+            return mapper.Map<IEnumerable<EmployeeBase>>(ds.Employees);
+        }
+
+        public EmployeeBase EmployeeGetById(int? id)
+        {
+            var e = ds.Employees.Find(id.GetValueOrDefault());
+
+            return e == null ? null : mapper.Map<EmployeeBase>(e);
+        }
+
+        public EmployeeBase EmployeeAddNew(EmployeeAdd newItem)
+        {
+            var addedItem = ds.Employees.Add(mapper.Map<Employee>(newItem));
+            ds.SaveChanges();
+
+            return addedItem == null ? null : mapper.Map<EmployeeBase>(addedItem);
+        }
+    }
+}
